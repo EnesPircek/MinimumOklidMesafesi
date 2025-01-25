fc18445a71196a1085b4f60917cc789ad2f979d9
EnesPircek <enesemrepircek@gmail.com>
2025-01-25 10:24:10 +0300

    Öklid mesafesi tanıtıldı.

---
+++ OklidMesafesi.py
@@ -0,0 +1,4 @@
+Kodluyoruz ve IBM işbirliğinde düzenlenen IBM ile skillsBuild etkinliği kapsamında Minimum Öklid Mesafesi hesaplama uygulamasını gerçekleştireceğim.
+Minimum Öklid Mesafesi, Öklid uzayındaki iki nokta arasındaki sıradan düz çizgi mesafesidir. Minimum Öklid Mesafesi bulms formülü ise d = √(x₂-x₁)²+(y₂-y₁)² 
+şeklindedir. Bu formül ile, düzlemde veya üç boyutlu uzayda iki nokta arasındaki mesafeyi bulabilirsiniz. İlerleyen commitlerde uygulamanın detayları 
+anlatılacaktır.
